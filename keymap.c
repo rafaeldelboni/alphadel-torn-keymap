24fec5cece01cc5890e5b1a5b16cccecf1405aa1
Rafael Delboni <rafadelboni@gmail.com>
2021-04-04 11:56:12 -0300

    feat: Adds cedilla keymap

--- keymap.c
+++ keymap.c
@@ -18,8 +18,11 @@
 
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+  CEDILLA = SAFE_RANGE,
+};
+
 #define CTRLESC LCTL_T(KC_ESC)  // Hold for Left Ctrl, Tap for ESC
-#define ALTQUOT LALT_T(KC_QUOT) // Hold for Left Alt, Tap for Quote
 
 // clang-format off
 
@@ -28,11 +31,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      CTRLESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, ALTQUOT,
+      CTRLESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                 KC_LGUI,   MO(1),  KC_SPC,  KC_SPC,     KC_ENT,  KC_ENT,   MO(2), KC_RALT
+                                 KC_LGUI,   MO(1),  KC_SPC,  KC_SPC,     KC_ENT,  KC_ENT,   MO(2), KC_LALT
                              //`-----------------------------------'  `-----------------------------------'
   ),
 
@@ -42,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       CTRLESC,KC_VOLD ,KC_VOLU ,KC_MPRV ,KC_MNXT , KC_MPLY,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LALT,
+      KC_LSFT, XXXXXXX, XXXXXXX, CEDILLA, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                 KC_LGUI, _______,  KC_SPC,  KC_SPC,    KC_ENT,   KC_ENT,   MO(3), KC_RALT
+                                 KC_LGUI, _______,  KC_SPC,  KC_SPC,    KC_ENT,   KC_ENT,   MO(3), KC_LALT
                              //`-----------------------------------'  `-----------------------------------'
   ),
 
@@ -56,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_PSCR,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                 KC_LGUI,   MO(3),  KC_SPC,  KC_SPC,     KC_ENT,  KC_ENT, _______, KC_RALT
+                                 KC_LGUI,   MO(3),  KC_SPC,  KC_SPC,     KC_ENT,  KC_ENT, _______, KC_LALT
                              //`-----------------------------------'  `-----------------------------------'
   ),
 
@@ -68,16 +71,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX,   KC_P1,   KC_P2,   KC_P3, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                   KC_P0, _______,  KC_SPC,  KC_SPC,     KC_ENT,  KC_ENT, _______, KC_RALT
+                                   KC_P0, _______,  KC_SPC,  KC_SPC,     KC_ENT,  KC_ENT, _______, KC_LALT
                              //`-----------------------------------'  `-----------------------------------'
   )
 };
 
+// clang-format on
+
 const uint16_t PROGMEM encoder_keymaps[][2][2] = {
   { { KC_PGDN, KC_PGUP }, { KC__VOLDOWN, KC__VOLUP } }
 };
-
-// clang-format on
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     torn_set_led(0, IS_LAYER_ON_STATE(state, 1));
@@ -85,3 +88,14 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     torn_set_led(2, IS_LAYER_ON_STATE(state, 3));
     return state;
 }
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case CEDILLA:
+        if (record->event.pressed) {
+           SEND_STRING(SS_ALGR(",")); // selects all and copies
+        }
+        break;
+    }
+    return true;
+};
